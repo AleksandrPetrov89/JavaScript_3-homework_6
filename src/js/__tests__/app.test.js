df7d422cc61f1722b8d97b5d28a0c620ecf223b8
AleksandrPetrov89 <petrovav89@gmail.com>
2025-01-03 11:44:40 +0500

    Prepared the project

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,9 @@
+// describe("Пример теста", () => {
+//   test.each([
+//     { str: "Hello!", expected: "Demo: Hello!" },
+//     { str: "", expected: "Demo: " },
+//     { str: 100, expected: "Demo: 100" },
+//   ])("demo($str)", ({ str, expected }) => {
+//     expect(demo(str)).toBe(expected);
+//   });
+// });
