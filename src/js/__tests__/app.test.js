5738475473bf719b6b49cb9dcaf894ee8be42466
AleksandrPetrov89 <petrovav89@gmail.com>
2025-01-12 20:28:01 +0300

    Made a save in localStorage

--- src/js/__tests__/app.test.js
+++
@@ -1,9 +0,0 @@
-// describe("Пример теста", () => {
-//   test.each([
-//     { str: "Hello!", expected: "Demo: Hello!" },
-//     { str: "", expected: "Demo: " },
-//     { str: 100, expected: "Demo: 100" },
-//   ])("demo($str)", ({ str, expected }) => {
-//     expect(demo(str)).toBe(expected);
-//   });
-// });
